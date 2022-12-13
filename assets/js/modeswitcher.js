33407e8e094fa73609958a29027dd590ee815f7c
sschu48 <33527878+sschu48@users.noreply.github.com>
2022-12-13 10:59:07 -0700

    Logo works in modeswitcher.js

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -13,26 +13,38 @@ let theme = sessionStorage.getItem('theme');
 const iconSun = "{{ site.baseurl }}/assets/img/sun.svg";
 const iconMoon = "{{ site.baseurl }}/assets/img/moon.svg";
 
+const iconWhiteLogo = "{{ site.baseurl }}/assets/img/logo-white.svg";
+const iconBlackLogo = "{{ site.baseurl }}/assets/img/logo-black.svg";
+
 
 function changeIconImgSrc(src) {
 	document.getElementById("theme-toggle-img").src = src;
 	document.getElementById("theme-toggle-img--mobile").src = src;
 }
 
+function changeLogoImgSrc(src) {
+	document.getElementById("theme-toggle-img-logo").src = src;
+}
+
 if (systemInitiatedDark.matches) {
 	changeIconImgSrc(iconMoon);
+	changeLogoImgSrc(iconWhiteLogo);
+	
 } else {
 	changeIconImgSrc(iconSun);
+	changeLogoImgSrc(iconBlackLogo);
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
    	changeIconImgSrc(iconMoon);
+	changeLogoImgSrc(iconWhiteLogo);
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
     changeIconImgSrc(iconSun);
+	changeLogoImgSrc(iconBlackLogo);
     sessionStorage.setItem('theme', '');
   }
 }
@@ -45,18 +57,22 @@ function modeSwitcher() {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
 		changeIconImgSrc(iconSun);
+		changeLogoImgSrc(iconBlackLogo);
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		changeIconImgSrc(iconMoon);
+		changeLogoImgSrc(iconWhiteLogo);
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
+		changeLogoImgSrc(iconBlackLogo);
 		changeIconImgSrc(iconSun);
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		changeIconImgSrc(iconMoon);
+		changeIconImgSrc(iconWhiteLogo);
 	}
 }
 
@@ -64,8 +80,10 @@ if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
 	changeIconImgSrc(iconMoon);
+	changeIconImgSrc(iconWhiteLogo);
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
 	changeIconImgSrc(iconSun);
+	changeLogoImgSrc(iconBlackLogo);
 }
